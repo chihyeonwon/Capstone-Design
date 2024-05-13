ec56f327888ed236c5234f092d0bd0ed86a51709
ylose <heungeob1003@gmail.com>
2024-05-13 17:43:14 +0900

    24.05.13 Push 클래스

--- app/src/main/java/com/example/giveback/utils/FcmPush.kt
+++ app/src/main/java/com/example/giveback/utils/FcmPush.kt
@@ -5,6 +5,7 @@ import com.example.giveback.PushDTO
 import com.google.common.net.MediaType
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.getValue
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.gson.Gson
 import okhttp3.Call
 import okhttp3.Callback
@@ -18,58 +19,52 @@ import okio.IOException
 import org.json.JSONObject
 
 // 푸시를 전송해주는 클래스
-class FcmPush {
-
-    // 헤더값
+class FcmPush() {
     val JSON = MediaType.parse("application/json; charset=utf-8")
-
-    // 푸시를 보낼 값 (Firebase 공식문서의 Legacy HTTP Server Protocol 주소)
     val url = "https://fcm.googleapis.com/fcm/send"
-
     val serverKey = "AAAA1l3beaI:APA91bGyEnn6KtvnUzoFr-Nwx18oRd0CSl1SwVGhr1YISMVBfdgwqA37d-2DB8EQykA44rpNbSgKiwURNB74XEstviIOpcTGwqIjESseBSPpMrYNv2TlDY4b-0eMt7XBf7iJYVhoLuuM"
 
-    var gson: Gson? = null
-
     var okHttpClient: OkHttpClient? = null
+    var gson: Gson? = null
+    init {
+        gson = Gson()
+        okHttpClient = OkHttpClient()
+    }
 
     // 싱글톤 패턴으로 어디에서나 사용가능하게 선언
     companion object {
         val instance = FcmPush()
     }
 
-    init {
-        // gson, okhttp 초기화
-        gson = Gson()
-        okHttpClient = OkHttpClient()
-    }
-
     fun sendMessage(destinationUid: String, title: String, message: String) {
-        FirebaseDatabase.getInstance().getReference("pushtokens").child(destinationUid).get().addOnCompleteListener { task ->
+        FirebaseFirestore.getInstance().collection("pushtokens").document(destinationUid).get().addOnCompleteListener { task ->
             if (task.isSuccessful) {
-                val token = task?.result.toString()
+                var token = task?.result?.get("pushToken").toString()
 
-                val pushDTO = PushDTO()
+                var pushDTO = PushDTO()
                 pushDTO.to = token
-                pushDTO.notification.title = title
-                pushDTO.notification.body = message
+                pushDTO.notification?.title = title
+                pushDTO.notification?.body = message
 
-                val jsonObject = JSONObject()
+                val jsonObject = gson?.toJson(pushDTO)
 
                 val body = jsonObject.toString().toRequestBody("application/json; charset=utf-8".toMediaType())
-                val request = Request.Builder()
+                var request = Request
+                    .Builder()
                     .addHeader("Content-Type", "application/json")
-                    .addHeader("Authorization", "key="+serverKey) // OAuth2 토큰 추가
+                    .addHeader("Authorization", "key=" + serverKey)
                     .url(url)
                     .post(body)
                     .build()
 
                 okHttpClient?.newCall(request)?.enqueue(object : Callback {
+
                     override fun onFailure(call: Call, e: IOException) {
-                        // 실패 처리
+
                     }
 
                     override fun onResponse(call: Call, response: Response) {
-                        println(response.body?.string())
+                        println(response?.body?.string())
                     }
                 })
             }
