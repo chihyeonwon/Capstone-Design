1ec64e4a4d26de4915ad01a6fa358718aee29b05
ylose <heungeob1003@gmail.com>
2024-06-05 02:45:23 +0900

    24.06.05 이미지 업로드 개수 제한

--- app/src/main/java/com/example/giveback/GetBoard/GetViewPagerAdapter.kt
+++ app/src/main/java/com/example/giveback/GetBoard/GetViewPagerAdapter.kt
@@ -1,6 +1,6 @@
 package com.example.giveback.GetBoard
 
-import android.media.Image
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,53 +9,70 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.giveback.R
-import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
-class GetViewPagerAdapter(val boardKeyList: MutableList<String>, val key: String, val count: Int) :
-    RecyclerView.Adapter<GetViewPagerAdapter.PagerViewHolder>() {
+class GetViewPagerAdapter(
+    var context: Context,
+    val boardKeyList: MutableList<String>,
+    val key: String,
+) :
+    RecyclerView.Adapter<GetViewPagerAdapter.ItemViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PagerViewHolder((parent))
+    inner class ItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return ItemViewHolder(
+            inflater.inflate(
+                R.layout.image_layout,
+                parent,
+                false
+            )
+        )
+    }
 
     override fun getItemCount(): Int = boardKeyList.size
 
-    override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
 
         // 사진을 차례대로 받아서 이미지뷰0~5번에 뿌림
-        for (i in 0 until count) {
+        for (i in 0 until 5) {
 
             val storageReference = Firebase.storage.reference.child("${key}${i}.png")
 
-            val imageViewFromFB = holder.Image
+            val imageViewFromFB = holder.itemView.findViewById<ImageView>(R.id.getImageArea)
+
 
             storageReference.downloadUrl.addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    val downloadUrl = task.result
                     if (imageViewFromFB != null) {
-                        Glide.with(holder.itemView)
-                            .load(downloadUrl)
-                            .into(imageViewFromFB)
+                        CoroutineScope(Dispatchers.IO).launch {
+                            val downloadUrl = storageReference.downloadUrl.await()
+                            withContext(Dispatchers.Main) {
+                                Glide.with(context)
+                                    .load(downloadUrl)
+                                    .override(300, 200) // 이미지 사이즈
+                                    .skipMemoryCache(false) // 메모리에 캐싱하려면 false
+                                    .diskCacheStrategy(DiskCacheStrategy.ALL) // 모든 이미지를 캐싱(기본값)
+                                    .thumbnail(
+                                        Glide.with(context)
+                                            .load(R.drawable.loading) // loading은 GIF 파일
+                                    ) // Glide로 이미지 로딩을 시작하기 전에 보여줄 이미지
+                                    .error(R.drawable.loading) //리소스를 불러오다가 에러 발생 시 보여줄 이미지
+                                    .into(imageViewFromFB)
+                            }
+                        }
                     }
                 } else {
-                    imageViewFromFB?.visibility = View.GONE
+
                 }
             }
-            holder.Image.setImageResource(
-                holder.itemView.resources.getIdentifier(
-                    "getImageArea",
-                    "id",
-                    "com.example.giveback"
-                )
-            ) as ImageView
         }
-    }
-
-    inner class PagerViewHolder(parent: ViewGroup) :
-        RecyclerView.ViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.image_layout, parent, false)
-        ) {
-
-        var Image = itemView.findViewById<ImageView>(R.id.getImageArea)!!
     }
 }
