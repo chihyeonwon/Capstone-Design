c76bd34430391e9e520fab4546267fbf386a5cf2
ylose <heungeob1003@gmail.com>
2024-06-05 03:41:15 +0900

    24.06.05 뷰페이저2 이미지뷰 오류 수정

--- app/src/main/java/com/example/giveback/GetBoard/GetViewPagerAdapter.kt
+++ app/src/main/java/com/example/giveback/GetBoard/GetViewPagerAdapter.kt
@@ -40,38 +40,28 @@ class GetViewPagerAdapter(
     override fun getItemCount(): Int = boardKeyList.size
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val storageReference = Firebase.storage.reference.child("${key}${position}.png")
+        val imageViewFromFB = holder.itemView.findViewById<ImageView>(R.id.getImageArea)
 
-        // 사진을 차례대로 받아서 이미지뷰0~5번에 뿌림
-        for (i in 0 until 5) {
-
-            val storageReference = Firebase.storage.reference.child("${key}${i}.png")
-
-            val imageViewFromFB = holder.itemView.findViewById<ImageView>(R.id.getImageArea)
-
-
-            storageReference.downloadUrl.addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    if (imageViewFromFB != null) {
-                        CoroutineScope(Dispatchers.IO).launch {
-                            val downloadUrl = storageReference.downloadUrl.await()
-                            withContext(Dispatchers.Main) {
+        storageReference.downloadUrl.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val downloadUrl = storageReference.downloadUrl.await()
+                    withContext(Dispatchers.Main) {
+                        Glide.with(context)
+                            .load(downloadUrl)
+                            .override(300, 200) // 이미지 사이즈
+                            .skipMemoryCache(false) // 메모리에 캐싱하려면 false
+                            .thumbnail(
                                 Glide.with(context)
-                                    .load(downloadUrl)
-                                    .override(300, 200) // 이미지 사이즈
-                                    .skipMemoryCache(false) // 메모리에 캐싱하려면 false
-                                    .diskCacheStrategy(DiskCacheStrategy.ALL) // 모든 이미지를 캐싱(기본값)
-                                    .thumbnail(
-                                        Glide.with(context)
-                                            .load(R.drawable.loading) // loading은 GIF 파일
-                                    ) // Glide로 이미지 로딩을 시작하기 전에 보여줄 이미지
-                                    .error(R.drawable.loading) //리소스를 불러오다가 에러 발생 시 보여줄 이미지
-                                    .into(imageViewFromFB)
-                            }
-                        }
+                                    .load(R.drawable.loading) // loading은 GIF 파일
+                            ) // Glide로 이미지 로딩을 시작하기 전에 보여줄 이미지
+                            .error(R.drawable.loading) // 리소스를 불러오다가 에러 발생 시 보여줄 이미지
+                            .into(imageViewFromFB)
                     }
-                } else {
-
                 }
+            } else {
+                // 에러 처리를 여기에 추가하세요 (예: 기본 이미지 표시)
             }
         }
     }
