ecfa9036578be19631539dca2e91997f48a8e037
ylose <heungeob1003@gmail.com>
2024-06-10 04:42:58 +0900

    24.06.10 관리자 댓글 알림 기능 추가

--- app/src/main/java/com/wonchihyeon/giveback/QnABoard/QnaBoardInsideActivity.kt
+++ app/src/main/java/com/wonchihyeon/giveback/QnABoard/QnaBoardInsideActivity.kt
@@ -5,11 +5,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import com.wonchihyeon.giveback.R
@@ -22,6 +25,8 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import com.wonchihyeon.giveback.utils.FcmPush
+import com.wonchihyeon.giveback.utils.PreferenceUtil
 
 // 게시글 보기 페이지
 class QnaBoardInsideActivity : AppCompatActivity() {
@@ -30,15 +35,17 @@ class QnaBoardInsideActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityQnaBoardInsideBinding
 
-    private lateinit var key:String
+    private lateinit var key: String
 
     private val commentDataList = mutableListOf<CommentModel>()
 
-    private lateinit var commentAdapter :CommentLVAdpater
+    private lateinit var commentAdapter: CommentLVAdpater
 
     val user = FirebaseAuth.getInstance().currentUser
 
     val email = user?.email.toString()
+
+    private lateinit var writerUid: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,9 +53,8 @@ class QnaBoardInsideActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_qna_board_inside)
 
-        // 관리자일 경우에만 댓글 입력할 수 있는 창과 댓글 리스트가 보이도록 수정
-        if(email == "heungeob1003@gwnu.myplug.kr") {
-            findViewById<ListView>(R.id.commentLV).isVisible = true // 댓글 리스트를 보이게 수정
+        // 관리자일 경우에만 댓글 입력할 수 있는 창이 보이도록 수정
+        if (email == "heungeob1003@gwnu.myplug.kr") {
             findViewById<LinearLayout>(R.id.writeComment).isVisible = true // 댓글 입력 레이아웃을 보이게 수정
         }
 
@@ -58,28 +64,31 @@ class QnaBoardInsideActivity : AppCompatActivity() {
         }
 
         key = intent.getStringExtra("key").toString()
+
+        getCommentData(key)
+
+        writerUid = intent.getStringExtra("uid").toString()
+
         getBoardData(key)
+
+        commentAdapter = CommentLVAdpater(commentDataList, key, writerUid)
+        binding.commentLV.adapter = commentAdapter
 
         // 댓글 입력 버튼을 누르면 댓글을 데이터베이스에 넣는다.
         binding.commentBtn.setOnClickListener {
             insertComment(key)
         }
-
-        getCommentData(key)
-
-        commentAdapter = CommentLVAdpater(commentDataList)
-        binding.commentLV.adapter = commentAdapter
     }
 
     // 댓글 데이터를 받아오는 함수
     fun getCommentData(key: String) {
-        val postListner = object: ValueEventListener {
+        val postListner = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
 
                 commentDataList.clear()
 
                 // dataModel에 있는 데이터를 하나씩 가져오는 부분
-                for(dataModel in dataSnapshot.children) {
+                for (dataModel in dataSnapshot.children) {
                     val item = dataModel.getValue(CommentModel::class.java)
                     commentDataList.add(item!!)
                 }
@@ -107,8 +116,15 @@ class QnaBoardInsideActivity : AppCompatActivity() {
                 )
             )
 
+        // 글 작성자에게 알림을 보낸다.
+        FcmPush.instance.sendMessage(
+            writerUid.toString(),
+            "관리자님이 답변을 달았습니다.",
+            "${binding.commentArea.text.toString()}"
+        )
+
         // 댓글 입력 완료 토스트 메시지를 띄운다.
-        Toast.makeText(this,"댓글 입력 완료",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "댓글 입력 완료", Toast.LENGTH_SHORT).show()
         // 댓글 입력 후에 입력창의 텍스트를 지운다.
         binding.commentArea.setText("")
     }
@@ -123,7 +139,7 @@ class QnaBoardInsideActivity : AppCompatActivity() {
         val alertDialog = mBuilder.show()
         // 수정버튼을 클릭했을 때
         alertDialog.findViewById<Button>(R.id.editBtn)?.setOnClickListener {
-            Toast.makeText(this,"수정 버튼을 눌렀습니다.",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "수정 버튼을 눌렀습니다.", Toast.LENGTH_SHORT).show()
 
             val intent = Intent(this, QnaBoardEditActivity::class.java)
             intent.putExtra("key", key)
@@ -132,7 +148,7 @@ class QnaBoardInsideActivity : AppCompatActivity() {
         // 삭제버튼을 클릭했을 때
         alertDialog.findViewById<Button>(R.id.removeBtn)?.setOnClickListener {
             FBRef.qnaboardRef.child(key).removeValue()
-            Toast.makeText(this,"삭제완료",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "삭제완료", Toast.LENGTH_SHORT).show()
 
             finish()
         }
@@ -140,18 +156,21 @@ class QnaBoardInsideActivity : AppCompatActivity() {
 
     // 게시글 데이터를 가져오는 함수
     private fun getBoardData(key: String) {
-        val postListner = object: ValueEventListener {
+        val postListner = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
 
                 val dataModel = dataSnapshot.getValue(QnaBoardModel::class.java)
                 if (dataModel != null) {
                     binding.titleArea.setText(dataModel.title)
                     binding.textArea.setText(dataModel.content)
+                    writerUid = dataModel?.uid.toString()
+                } else {
+                    writerUid = "abc"
                 }
 
                 val myUid = FBAuth.getUid()
                 val writerUid = dataModel?.uid
-                if(myUid.equals(writerUid)){
+                if (myUid.equals(writerUid)) {
                     binding.boardSettingIcon.isVisible = true
                 } else {
 
--- app/src/main/java/com/wonchihyeon/giveback/comment/CommentLVAdpater.kt
+++ app/src/main/java/com/wonchihyeon/giveback/comment/CommentLVAdpater.kt
@@ -1,14 +1,21 @@
 package com.wonchihyeon.giveback.comment
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.wonchihyeon.giveback.R
+import com.wonchihyeon.giveback.utils.FBAuth
+import com.wonchihyeon.giveback.utils.FBRef
+import com.wonchihyeon.giveback.utils.FcmPush
 
 // 리스트뷰와 댓글 데이터를 연결해주는 어댑터
-class CommentLVAdpater(val commentList:MutableList<CommentModel>) :  BaseAdapter() {
+class CommentLVAdpater(val commentList:MutableList<CommentModel>, val key: String, val uid: String) :  BaseAdapter() {
     override fun getCount(): Int {
         return commentList.size
     }
